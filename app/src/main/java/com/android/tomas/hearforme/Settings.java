8c1661427c72eeaf4b013b2fc6554574b919497f
tomasroigmartinez <tomroima@inf.upv.es>
2014-10-07 14:41:34 +0200

    Changing Settings to Android standard.

--- app/src/main/java/com/android/tomas/hearforme/Settings.java
+++ app/src/main/java/com/android/tomas/hearforme/Settings.java
@@ -1,16 +1,14 @@
 package com.android.tomas.hearforme;
 
 import android.app.Activity;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
+
+import android.preference.PreferenceFragment;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.view.View;
 
-import java.io.Console;
-import java.util.logging.Logger;
 
 
 public class Settings extends Activity {
@@ -20,9 +18,11 @@ public class Settings extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
-
+ /*
     @Override
     protected void onResume() {
         super.onResume();
@@ -30,15 +30,15 @@ public class Settings extends Activity {
         Global g = (Global) getApplication();
         text.setTextSize(TypedValue.COMPLEX_UNIT_PX, g.getTextSize());
     }
-
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
-
-    public void magnify(View v){
+    /*
+    public void magnify(View v) {
         TextView text = (TextView) findViewById(R.id.textView);
         Log.d(TAG, String.valueOf(text.getTextSize() ));
 
@@ -57,6 +57,7 @@ public class Settings extends Activity {
         Global g = (Global) getApplication();
         g.setTextSize(text.getTextSize());
     }
+    */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -68,4 +69,17 @@ public class Settings extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
+
+    }
+
 }

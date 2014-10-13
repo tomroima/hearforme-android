343fcd5ff7019ea5e0dd9ec052f887800e56f201
tomasroigmartinez <tomroima@inf.upv.es>
2014-10-13 13:28:46 +0200

    The end 2.0

--- app/src/main/java/com/android/tomas/hearforme/CustomDialogPreference.java
+++ app/src/main/java/com/android/tomas/hearforme/CustomDialogPreference.java
@@ -14,12 +14,12 @@ import android.widget.TextView;
 /**
  * Created by tomasroigmartinez on 07/10/14.
  */
-public class TheCustomDialogPreference extends DialogPreference {
+public class CustomDialogPreference extends DialogPreference {
     private static final String TAG = Settings.class.getSimpleName();
 
     private int mTextSize;
 
-        public TheCustomDialogPreference(Context context, AttributeSet attrs) {
+        public CustomDialogPreference(Context context, AttributeSet attrs) {
             super(context, attrs);
             setDialogLayoutResource(R.layout.preferences_custom_dialog);
 
